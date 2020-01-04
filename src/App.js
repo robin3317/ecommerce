d2d0ec3c50abe941ea1c9b470a9404307acb6001
robin3317 <abdurrahmanrobin47@gmail.com>
2020-01-04 22:54:39 +0600

    header adds

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/Shop';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
