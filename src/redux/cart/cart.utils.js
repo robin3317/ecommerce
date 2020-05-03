8459b1aa26d756e6c257d72466b288f7e31bb8bc
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2020-05-03 23:23:22 +0600

    increase and decrease cart item

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,10 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToAdd.id
+    (cartItem) => cartItem.id === cartItemToAdd.id
   );
 
   if (existingCartItem) {
-    return cartItems.map(cartItem =>
+    return cartItems.map((cartItem) =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 }
         : cartItem
@@ -12,4 +12,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem.quantity == 1) {
+    return cartItems.filter((cartItem) => cartItem.id != existingCartItem.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToAdd.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
