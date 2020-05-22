8e7ba0d734c6c61fa51d5909302b01f4fc82f9b8
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2020-05-23 00:55:52 +0600

    nested route setup

---
+++ src/pages/collection/Collection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CollectionPage = ({ match }) => {
+  return (
+    <div className="category">
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  );
+};
+
+export default CollectionPage;
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import CollectionOverview from '../../components/collection-overview/CollectionOverview';
+import { Route } from 'react-router-dom';
+import CollectionPage from '../collection/Collection';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
